cf6bd12fa960b11a3a371b182306919d0cbf7a32
Saurabh Mishra <saurabhmishra@Saurabh-Macbook.local>
2020-03-05 22:35:47 +0530

    build_files_added

---
+++ dist/precache-manifest.3e10db1d42d4a2e83b8104d40bbebde2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85b5a2a6ebcc8b4e4a5",
+    "url": "/css/app.429f886a.css"
+  },
+  {
+    "revision": "8ac3d18b2099a1892f13",
+    "url": "/css/chunk-vendors.b927d7f4.css"
+  },
+  {
+    "revision": "476fb656081a44cc8a76415ae0cd57aa",
+    "url": "/img/success.476fb656.svg"
+  },
+  {
+    "revision": "aede550ebdfb266f6c404aafa6439e60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85b5a2a6ebcc8b4e4a5",
+    "url": "/js/app.633e16ee.js"
+  },
+  {
+    "revision": "8ac3d18b2099a1892f13",
+    "url": "/js/chunk-vendors.9301ef3c.js"
+  },
+  {
+    "revision": "861e5a06b36223759ea6cc9d8556d098",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
